81f3b3c888a57ffd490109f9ef8e6227e3eaf52e
David Foster <david.foster@tylertech.com>
2022-06-21 09:29:15 -0400

    add sln function

--- PruneLocal.psm1
+++ PruneLocal.psm1
@@ -17,3 +17,27 @@ Function PruneLocalHard {
 	git remote prune origin; 
 	git branch -vv | Select-String -Pattern ": gone]" | % { $_.toString().Trim().Split(" ")[0]} | % {git branch -D $_}
 }
+
+# Run sln (an alias for Open-Solution) to recursively search your current working 
+# directory for a solution file and launch it, if one is found.
+# Copied from https://github.com/refactorsaurusrex/whats-new
+function Open-Solution {
+  [Alias('sln')]
+  param (
+    [string]$RootDirectory = $PWD
+  )
+
+  $solutions = Get-ChildItem -Recurse -Path $RootDirectory -Filter "*.sln"
+  if ($solutions.Count -eq 1) {
+    Invoke-Item $solutions.FullName
+  }
+  elseif ($solutions.Count -eq 0) {
+    Write-Host "I couldn't find any solution files here!" -ForegroundColor Red
+  }
+  elseif ($solutions.Count -gt 1) {
+    Write-Host "I found more than 1 solution. Which one do you want to open?" -ForegroundColor Yellow
+    $solutions | Format-Table @{ Label="Solutions"; Expression={" --> $_"} }
+  }
+}
+
+Export-ModuleMember -Function * -Alias *
